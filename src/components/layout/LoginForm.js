425e8fc3722400742e1095c0413dc3b14a649e8f
Joel Bermudez Acosta <47182906+JoelBerus@users.noreply.github.com>
2021-12-29 13:21:58 -0500

    Update LoginForm.js

--- src/components/layout/LoginForm.js
+++ src/components/layout/LoginForm.js
@@ -1,10 +1,82 @@
 import * as React from 'react';
+import { useFormik } from 'formik';
 import useForm from '../../hooks/useForm';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Input from '../common/Input';
 
+const validate = (values) => {
+
+    const errors = {}
+
+    if(!values.name) {
+        errors.username = 'User name required';
+    }
+
+    if(!values.password) {
+        errors.password = 'Password required';
+    }
+
+    return errors
+}
+
 function LoginForm() {
+
+    const data = {
+        username: 'admin',
+        password: 'admin'
+    }
+
+    React.useEffect(() => {
+        // apiFecth({...data});
+    }, []);
+
+    const apiFecth = async({username, password }) => {
+        console.log(username, password);
+        
+        // const url = 'http://192.168.200.102:8080/rest/usuarios/usuarios.php';
+        const url = 'https://jsonplaceholder.typicode.com/posts';
+        const requestOption  = {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(
+                {
+                    // "tipo":"login_cliente",
+                    "body":`${username}`,
+                    "title":`${password}`,
+                }
+            )
+        };
+        const response = await fetch(url, requestOption);
+        const data = await response.json();
+        console.log(data.id);
+
+    }
+    
+    const formik = useFormik({
+        initialValues: {
+            username: '',
+            email: '',
+            password: '',
+        },
+        validate: values => {
+            const errors = {};
+            if (!values.username) {
+                errors.username = 'User name required';
+            }
+            if (!values.password) {
+                errors.password = 'Password required';
+            }
+            return errors;
+        },
+        onSubmit: values => {
+            // console.log(values);
+            apiFecth({...values});// apiFecth({values.username, values.password});
+        }
+    })
 
     const [value, handleChange] = useForm({ username:'', password:'' });
     
@@ -19,10 +91,9 @@ function LoginForm() {
     };
     
     return (
-        <form onSubmit={handleSubmit} action='/about'>
+        <form onSubmit={formik.handleSubmit} action='/about'>
             <div>
                 <Input
-                    value={value.username}
                     id="username"
                     label="User name"
                     name="username"
@@ -30,11 +101,13 @@ function LoginForm() {
                     required
                     fullWidth
                     autoFocus    
-                    onChange={handleChange}
+                    value={formik.values.username}
+                    onBlur={formik.onBlur}
+                    onChange={formik.handleChange}
+                    // {...(formik.errors.username && { error: formik.errors.username })}  
                     // error="Field is required"
                 />
                 <TextField
-                    value={value.password}
                     name="password"
                     label="Password"
                     type="password"
@@ -42,7 +115,10 @@ function LoginForm() {
                     autoComplete="current-password"
                     required
                     fullWidth
-                    onChange={handleChange}
+                    value={formik.values.password}
+                    onBlur={formik.onBlur}
+                    onChange={formik.handleChange}
+                    // {...(formik.errors.password && { error: formik.errors.password })}
                     // error="Field is required"
                 />
                 <Button
